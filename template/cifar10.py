9dbb619c3be0e904f77c63d3ad2218c2dc8ac6d8
xiaojiuwo168 <85736050+Xiaojiuwo168@users.noreply.github.com>
2022-12-08 09:22:11 +0800

    Update cifar10.py

--- template/cifar10.py
+++ template/cifar10.py
@@ -12,6 +12,7 @@ from datetime import datetime
 import multiprocessing
 from utils import StatusUpdateTool
 
+# choice 1: network without skip connections
 class BasicBlock(nn.Module):
     expansion = 1
     def __init__(self, in_planes, planes, stride=1):
@@ -26,6 +27,29 @@ class BasicBlock(nn.Module):
         out = self.bn2(self.conv2(out))
         out = F.relu(out)
         return out
+
+# choice 2: network with skip connections
+# class BasicBlock(nn.Module):
+#     expansion = 1
+#     def __init__(self, in_planes, planes, stride=1):
+#         super(BasicBlock, self).__init__()
+#         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+#         self.bn1 = nn.BatchNorm2d(planes)
+#         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+#         self.bn2 = nn.BatchNorm2d(planes)
+#
+#         self.shortcut = nn.Sequential()
+#         if stride !=1 or in_planes != self.expansion*planes:
+#             self.shortcut = nn.Sequential(
+#                 nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+#                 nn.BatchNorm2d(self.expansion*planes)
+#             )
+#     def forward(self, x):
+#         out = F.relu(self.bn1(self.conv1(x)))
+#         out = self.bn2(self.conv2(out))
+#         out += self.shortcut(x)
+#         out = F.relu(out)
+#         return out
 
 
 class EvoCNNModel(nn.Module):
